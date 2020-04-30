63d0264d641f244880837bf57e4920745bf2c27f
Andrew Reusch <areusch@octoml.ai>
2020-04-30 11:11:31 -0700

    inital commit for on-device runtime tests

--- main.cpp
+++ main.cpp
@@ -5,16 +5,17 @@
 
 #include "mbed.h"
 
-#define WAIT_TIME_MS 500 
+#define WAIT_TIME_MS 500
 DigitalOut led1(LED1);
 
 int main()
 {
     printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+    printf("System Clock: %ld\n", SystemCoreClock);
 
     while (true)
     {
         led1 = !led1;
-        thread_sleep_for(WAIT_TIME_MS);
+//        thread_sleep_for(WAIT_TIME_MS);
     }
 }
