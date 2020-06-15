b21f062ddf23945914d88f3bfc7b5f101dbcb7f4
Andrew Reusch <areusch@octoml.ai>
2020-06-15 12:23:14 -0700

    crt

--- main.cpp
+++ main.cpp
@@ -5,23 +5,52 @@
 
 #include "mbed.h"
 
+#include <cstdio>
+#include <tvm/runtime/crt/logging.h>
+#include <tvm/runtime/micro/micro_rpc_server.h>
+
 #define WAIT_TIME_MS 500
 DigitalOut led1(LED1);
+
+extern "C" {
+ssize_t write_serial(void* unused_context, const uint8_t* data, size_t size) {
+  ssize_t to_return = write(fileno(stdout), data, size);
+  if (to_return > 0) {
+    fflush(stdout);
+    fsync(STDOUT_FILENO);
+  }
+
+  return to_return;
+}
+
+void TVMPlatformAbort(int error) {
+  for (;;) ;
+}
+}
+
+constexpr const size_t kWorkspaceSizeBytes = 60 * 1024;
+constexpr const size_t kWorkspacePageSizeBytesLog2 = 8;
+
+uint8_t workspace[kWorkspaceSizeBytes];
+
 
 int main()
 {
     led1 = 1;
-    asm volatile("bkpt");
-    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
-    printf("System Clock: %ld\n", SystemCoreClock);
+    utvm_rpc_server_t server = utvm_rpc_server_init(workspace, kWorkspaceSizeBytes, kWorkspacePageSizeBytesLog2,
+                                                    write_serial, nullptr);
+    TVMLogf("uTVM On-Device Runtime");
 
     while (true)
     {
         led1 = !led1;
-        const unsigned int num_loops =
-          SystemCoreClock / 4  // loops per second, with 4x fudge factor
-          / 1000  // loops per ms
-          * 500;  // go for 500ms
-        for (volatile unsigned int i = 0; i < num_loops; i++) ;
+        uint8_t c;
+        ssize_t bytes_read = read(fileno(stdin), &c, 1);
+        if (bytes_read > 0) {
+          TVMLogf("r: %02x", c);
+          utvm_rpc_server_receive_byte(server, c);
+          utvm_rpc_server_loop(server);
+          TVMLogf("ld");
+        }
     }
 }
